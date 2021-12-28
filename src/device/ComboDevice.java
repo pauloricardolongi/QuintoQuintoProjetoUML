0046bca061d43775f2b6707f10031c321880d4ac
pauloricardolongi <pauloricardolongi@gmail.com>
2021-12-28 00:35:15 -0300

    ComboDevice

--- src/device/ComboDevice.java
+++ src/device/ComboDevice.java
@@ -1,5 +1,24 @@
 package device;
 
-public class ComboDevice {
+public class ComboDevice extends Device implements Scanner, Print{
+	
+	public ComboDevice(String serialNumber) {
+		super(serialNumber);
+	}
+
+	@Override
+	public void print(String doc) {
+		System.out.println("Combo printing: " + doc);
+	}
+
+	@Override
+	public String scan() {
+		return "Combo scan result";
+	}
+
+	@Override
+	public void processDoc(String doc) {
+		System.out.println("Combo processing: " + doc);
+	}
 
 }
